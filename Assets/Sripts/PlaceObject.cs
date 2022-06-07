4e168f54a698476b0963cf4a7b3e0f6b9cd57e88
majo camcam <mariajosecamberocambero.24@gmail.com>
2022-06-08 00:18:50 +0200

    Buttons scripts, 3d objects and PlaceObject script bool alreadycreated

--- Assets/Sripts/PlaceObject.cs
+++ Assets/Sripts/PlaceObject.cs
@@ -8,10 +8,12 @@ public class PlaceObject : MonoBehaviour
     public ARRaycastManager RayCastManager;
     public GameObject objectToCreate;
     List<ARRaycastHit> hits = new List<ARRaycastHit>();
+    bool alreadycreated;
+
 
     void Update()
     {
-        if(RayCastManager != null) {
+        if (RayCastManager != null) {
             Touch touch;
             if (Input.touchCount < 1 || (touch = Input.GetTouch(0)).phase != TouchPhase.Began) {
                 return;
@@ -23,8 +25,10 @@ public class PlaceObject : MonoBehaviour
 
             if (RayCastManager.Raycast(touch.position, hits))
             {
+                if (alreadycreated) { return; }
                 var hit = hits[0];
                 GameObject.Instantiate(objectToCreate, hit.pose.position, hit.pose.rotation);
+                alreadycreated = true;
             }
         } else {
             RayCastManager = FindObjectOfType<ARRaycastManager>();
